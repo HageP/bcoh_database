51bea7e78cc37c8038d41235990d9826732cbbe1
Raghu Betina <raghu@firstdraft.com>
2016-11-15 22:48:50 +0000

    Whitelist columns in Tblbuilding for ActiveAdmin

--- app/admin/tblbuilding.rb
+++ app/admin/tblbuilding.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Tblbuilding do
 
+ permit_params :buildingname, :buildingaddress, :buildingowner, :buildingzip, :buildingcity
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
