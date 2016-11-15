c0f5f872dbdc3c1b98e88879603af9f394d14caf
Raghu Betina <raghu@firstdraft.com>
2016-11-15 22:47:01 +0000

    Direct association between tbltenants and tblleases

--- app/models/tbllease.rb
+++ app/models/tbllease.rb
@@ -1,6 +1,9 @@
 class Tbllease < ApplicationRecord
   # Direct associations
 
+  belongs_to :tenant,
+             :class_name => "Tbltenant"
+
   belongs_to :apartment,
              :class_name => "Tblapartment"
 
--- app/models/tbltenant.rb
+++ app/models/tbltenant.rb
@@ -1,6 +1,9 @@
 class Tbltenant < ApplicationRecord
   # Direct associations
 
+  has_many   :tblleases,
+             :foreign_key => "tenant_id"
+
   # Indirect associations
 
   # Validations
