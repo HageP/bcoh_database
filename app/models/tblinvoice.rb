941f729357541c829e11a15d8859c8adff0e98f7
Raghu Betina <raghu@firstdraft.com>
2016-11-15 22:47:01 +0000

    Direct association between tblinvoices and tblpayments

--- app/models/tblinvoice.rb
+++ app/models/tblinvoice.rb
@@ -1,6 +1,10 @@
 class Tblinvoice < ApplicationRecord
   # Direct associations
 
+  has_many   :tblpayments,
+             :foreign_key => "invoice_id",
+             :dependent => :destroy
+
   belongs_to :tenant,
              :class_name => "Tbltenant"
 
--- app/models/tblpayment.rb
+++ app/models/tblpayment.rb
@@ -1,6 +1,9 @@
 class Tblpayment < ApplicationRecord
   # Direct associations
 
+  belongs_to :invoice,
+             :class_name => "Tblinvoice"
+
   # Indirect associations
 
   # Validations
