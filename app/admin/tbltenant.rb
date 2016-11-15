a69aceb115a588f5032623ca4ec2efb71c43d6c5
Raghu Betina <raghu@firstdraft.com>
2016-11-15 22:48:59 +0000

    Whitelist columns in Tbltenant for ActiveAdmin

--- app/admin/tbltenant.rb
+++ app/admin/tbltenant.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Tbltenant do
 
+ permit_params :tenant_first, :tenant_email, :tenant_phone
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
